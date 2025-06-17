bd2b3893f9341461e93f716f3ef57af93c5e9ec1
Sujithra S <sujithra.s2023@vitstudent.ac.in>
2025-06-17 19:15:12 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 57.3 MB (99.90%)

---
+++ 0189-rotate-array/solution.java
@@ -0,0 +1,28 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        
+        int n = nums.length;
+        k = k % n;  // In case k > n
+
+        // Step 1: Reverse entire array
+        reverse(nums, 0, n - 1);
+
+        // Step 2: Reverse first k elements
+        reverse(nums, 0, k - 1);
+
+        // Step 3: Reverse remaining elements
+        reverse(nums, k, n - 1);
+    }
+        private void reverse(int[] nums,int start,int end){
+            while(start<end){
+                int first=nums[start];
+                nums[start]=nums[end];
+                nums[end]=first;
+                start++;
+                end--;
+            }
+            
+        }
+        
+    
+}
