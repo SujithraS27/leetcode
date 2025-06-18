131d71f35e4112f10955c1ef96b9ba6ae24f3a01
Sujithra S <sujithra.s2023@vitstudent.ac.in>
2025-06-18 16:22:10 +0000

    Sync LeetCode submission Runtime - 1 ms (99.98%), Memory - 46.3 MB (99.26%)

---
+++ 0136-single-number/solution.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int miss=nums[0];
+        for(int i=1;i<nums.length;i++){
+            miss=miss^nums[i];
+        }
+        return miss;
+    }
+}
