616c0c1abd67b4c3209d5ccd01a93349d68507c6
Sujithra S <sujithra.s2023@vitstudent.ac.in>
2025-07-09 18:10:47 +0000

    Sync LeetCode submission Runtime - 683 ms (60.02%), Memory - 0.0B (100.00%)

---
+++ 2087-confirmation-rate/solution.sql
@@ -0,0 +1,2 @@
+SELECT s.user_id,COALESCE(ROUND(SUM(CASE WHEN c.action='confirmed' THEN 1 ELSE 0 END)/COUNT(c.action),2),0) as confirmation_rate FROM Signups s LEFT JOIN Confirmations c ON s.user_id=c.user_id GROUP BY s.user_id;
+
