31f842eb0d62a29d6aca55086c39980578aaa239
Sujithra S <sujithra.s2023@vitstudent.ac.in>
2025-08-26 17:08:43 +0000

    Sync LeetCode submission Runtime - 7 ms (40.44%), Memory - 18.4 MB (100.00%)

--- 0152-maximum-product-subarray/solution.py
+++ 0152-maximum-product-subarray/solution.py
@@ -1,12 +1,13 @@
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        curmin=nums[0]
-        curmax=nums[0]
-        maxprod=nums[0]
-        for i in (nums[1:]):
-            temp_max=max(i,i*curmax,i*curmin)
-            curmin=min(i,i*curmax,i*curmin)
-            curmax=temp_max
-            maxprod=max(maxprod,curmax)
-        return maxprod
-
+        res=max(nums)
+        curmax,curmin=1,1
+        for i in nums:
+            if(i==0):
+                curmax,curmin=1,1
+                continue
+            temp=curmax*i
+            curmax=max(temp,curmin*i,i)
+            curmin=min(temp,curmin*i,i)
+            res=max(res,curmax,curmin)
+        return res
