cd4fc59a919a053cb8e3368f9d2a443cfdec09d6
Sujithra S <sujithra.s2023@vitstudent.ac.in>
2025-08-26 19:03:39 +0000

    Sync LeetCode submission Runtime - 295 ms (99.79%), Memory - 0.0B (100.00%)

---
+++ 4025-find-loyal-customers/solution.sql
@@ -0,0 +1,2 @@
+WITH temp AS(SELECT customer_id,SUM(CASE WHEN transaction_type='purchase' THEN 1 ELSE 0 END) AS purchase_count,SUM(CASE WHEN transaction_type='refund' THEN 1 ELSE 0 END) AS refund_count,COUNT(*) AS total_transactions,DATEDIFF(MAX(transaction_date),MIN(transaction_date)) AS active FROM customer_transactions GROUP BY customer_id)
+SELECT customer_id FROM temp WHERE purchase_count>=3 AND active>=30 AND (refund_count*1.0/total_transactions)<0.2  ORDER BY customer_id;
