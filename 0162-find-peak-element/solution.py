3e73323ff250010892b13228644f142341e9e1f2
Sujithra S <sujithra.s2023@vitstudent.ac.in>
2025-09-06 14:37:01 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.9 MB (100.00%)

---
+++ 0162-find-peak-element/solution.py
@@ -0,0 +1,21 @@
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        n=len(nums)
+        if(n==1):
+            return 0
+        if(nums[0]>nums[1]):
+            return 0
+        if(nums[n-1]>nums[n-2]):
+            return n-1
+        start=1
+        end=len(nums)-2
+        while(start<=end):
+            mid=start+(end-start)//2
+            if(nums[mid]>nums[mid+1] and nums[mid]>nums[mid-1]):
+                return mid
+            if(nums[mid]>nums[mid-1]):
+                start=mid+1
+            else:
+                end=mid-1
+        return -1
+
