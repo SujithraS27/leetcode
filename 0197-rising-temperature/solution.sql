1e95fb08d8cdc54762768f93cd3bf5922c381513
Sujithra S <sujithra.s2023@vitstudent.ac.in>
2025-12-22 18:27:01 +0000

    Sync LeetCode submission Runtime - 364 ms (99.63%), Memory - 0.0B (100.00%)

--- 0197-rising-temperature/solution.sql
+++ 0197-rising-temperature/solution.sql
@@ -1,2 +1,2 @@
 # Write your MySQL query statement below
-SELECT id AS Id FROM (SELECT id,temperature,recordDate,LAG(temperature) OVER(ORDER BY recordDate) as prev_temp,LAG(recordDate) OVER (ORDER BY recordDate) AS prev_date FROM Weather) AS wea  WHERE temperature>prev_temp AND DATEDIFF(recordDate,prev_date)=1;
+SELECT id FROM (SELECT id,temperature,recordDate,LAG(temperature) OVER(ORDER BY recordDate) AS prev_temp,LAG(recordDate) OVER(ORDER BY recordDate) AS prev_date FROM Weather) AS w WHERE temperature>prev_temp AND DATEDIFF(recordDate,prev_date)=1
