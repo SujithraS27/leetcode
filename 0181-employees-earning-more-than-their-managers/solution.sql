17f2cfc8880da25ef6f06a31607890f3a146ea54
Sujithra S <sujithra.s2023@vitstudent.ac.in>
2026-01-05 15:47:50 +0000

    Sync LeetCode submission Runtime - 412 ms (36.32%), Memory - 0.0B (100.00%)

--- 0181-employees-earning-more-than-their-managers/solution.sql
+++ 0181-employees-earning-more-than-their-managers/solution.sql
@@ -1,2 +1,2 @@
-SELECT e.name AS Employee FROM Employee e INNER JOIN Employee m ON e.managerId=m.id WHERE e.salary>m.salary
-
+# Write your MySQL query statement below
+SELECT e.name AS Employee FROM Employee e JOIN Employee m ON e.managerId=m.id WHERE e.salary>m.salary
