2521b9fea579be3854e456abde1e72a6523a8eb5
Sujithra S <sujithra.s2023@vitstudent.ac.in>
2026-01-05 17:39:42 +0000

    Sync LeetCode submission Runtime - 5 ms (14.35%), Memory - 43.3 MB (57.46%)

---
+++ 0020-valid-parentheses/solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack=new Stack<>();
+        HashMap<Character,Character> map=new HashMap<>();
+        map.put(')','(');
+        map.put('}','{');
+        map.put(']','[');
+        for(char ch:s.toCharArray()){
+            if(map.containsValue(ch)){
+                stack.push(ch);
+            }
+            else if(map.containsKey(ch)){
+                if(stack.isEmpty() || map.get(ch)!=stack.pop()){
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
