cf225ea55febacffc83631d001547a9a480a241a
Sujithra S <sujithra.s2023@vitstudent.ac.in>
2026-01-05 16:06:45 +0000

    Sync LeetCode submission Runtime - 256 ms (99.18%), Memory - 0.0B (100.00%)

--- 0176-second-highest-salary/solution.sql
+++ 0176-second-highest-salary/solution.sql
@@ -1,6 +1,2 @@
 # Write your MySQL query statement below
-select
-(select distinct Salary 
-from Employee order by salary desc 
-limit 1 OFFSET 1 ) 
-as SecondHighestSalary;
+SELECT(SELECT DISTINCT salary FROM Employee ORDER BY salary DESC LIMIT 1 OFFSET 1)AS SecondHighestSalary
