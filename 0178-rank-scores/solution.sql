54a85ea0dd92432951a270fcab7823c5888b8e5a
Sujithra S <sujithra.s2023@vitstudent.ac.in>
2026-01-09 13:51:35 +0000

    Sync LeetCode submission Runtime - 281 ms (99.84%), Memory - 0.0B (100.00%)

--- 0178-rank-scores/solution.sql
+++ 0178-rank-scores/solution.sql
@@ -1,2 +1,2 @@
 # Write your MySQL query statement below
-SELECT score,DENSE_RANK() OVER(ORDER BY score DESC) AS 'rank' FROM Scores
+SELECT score,DENSE_RANK() OVER (ORDER BY score DESC) AS 'rank' FROM Scores 
