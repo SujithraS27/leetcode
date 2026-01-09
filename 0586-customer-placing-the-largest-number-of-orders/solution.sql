12072c0b918a05c4acd8d77f0213c6435541cdbb
Sujithra S <sujithra.s2023@vitstudent.ac.in>
2026-01-09 13:20:57 +0000

    Sync LeetCode submission Runtime - 511 ms (29.53%), Memory - 0.0B (100.00%)

--- 0586-customer-placing-the-largest-number-of-orders/solution.sql
+++ 0586-customer-placing-the-largest-number-of-orders/solution.sql
@@ -1,8 +1,2 @@
 # Write your MySQL query statement below
-SELECT customer_number
-FROM Orders
-GROUP BY customer_number
-ORDER BY COUNT(order_number) DESC
-LIMIT 1;
-
-
+SELECT customer_number FROM Orders GROUP BY customer_number ORDER BY COUNT(*) DESC LIMIT 1
