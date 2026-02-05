1d54f9a3aac4688029723e7dca607d27e8e4f51b
Sujithra S <sujithra.s2023@vitstudent.ac.in>
2026-02-05 18:02:42 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 47.7 MB (56.67%)

---
+++ 0042-trapping-rain-water/solution.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int trap(int[] height) {
+        int maxleft=0;
+        int maxright=0;
+        int totalwater=0;
+        int left=0;
+        int right=height.length-1;
+        while(left<right){
+            if(height[left]<height[right]){
+                if(height[left]>maxleft){
+                    maxleft=height[left];
+                }
+                totalwater=totalwater+(maxleft-height[left]);
+                left++;
+            }
+            else{
+                if(height[right]>maxright){
+                    maxright=height[right];
+                }
+                totalwater=totalwater+(maxright-height[right]);
+                right--;
+            }
+        }
+        return totalwater;
+    }
+}
