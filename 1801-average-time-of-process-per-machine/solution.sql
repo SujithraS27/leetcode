940ebb1545be9768a77cd6a2e14af4c1102338b2
Sujithra S <sujithra.s2023@vitstudent.ac.in>
2026-02-10 17:38:38 +0000

    Sync LeetCode submission Runtime - 249 ms (78.23%), Memory - 0.0B (100.00%)

--- 1801-average-time-of-process-per-machine/solution.sql
+++ 1801-average-time-of-process-per-machine/solution.sql
@@ -1,2 +1,13 @@
-# Write your MySQL query statement below
-SELECT machine_id,ROUND(SUM(CASE WHEN activity_type='start' THEN timestamp*-1 ELSE timestamp END)*1.0/(SELECT COUNT(DISTINCT process_id)),3) AS processing_time FROM activity GROUP BY machine_id;
+SELECT
+    machine_id,
+    ROUND(
+        AVG(
+            CASE
+                WHEN activity_type = 'end' THEN timestamp
+                ELSE -timestamp
+            END
+        )*2
+    , 3) AS processing_time
+FROM Activity
+GROUP BY machine_id;
+
