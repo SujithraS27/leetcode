fc57149746f1f495ad75b31fb9dcdf37899cfbf1
Sujithra S <sujithra.s2023@vitstudent.ac.in>
2026-02-10 19:27:19 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 42.9 MB (61.26%)

---
+++ 0054-spiral-matrix/solution.java
@@ -0,0 +1,34 @@
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        ArrayList<Integer> res=new ArrayList<>();
+        int m=matrix.length;
+        int n=matrix[0].length;
+        int top=0;
+        int bottom=m-1;
+        int left=0;
+        int right=n-1;
+        while(top<=bottom && left<=right){
+            for(int col=left;col<=right;col++){
+                res.add(matrix[top][col]);
+            }
+            top++;
+            for(int row=top;row<=bottom;row++){
+                res.add(matrix[row][right]);
+            }
+            right--;
+            if(top<=bottom){
+                for(int col=right;col>=left;col--){
+                    res.add(matrix[bottom][col]);
+                }
+                bottom--;
+            }
+            if(left<=right){
+                for(int row=bottom;row>=top;row--){
+                    res.add(matrix[row][left]);
+                }
+                left++;
+            }
+        }
+        return res;
+    }
+}
