a6fc431ad6f98d2319ff23351cffc6321b6807ca
Sujithra S <sujithra.s2023@vitstudent.ac.in>
2026-03-02 11:32:17 +0000

    Sync LeetCode submission Runtime - 3 ms (99.84%), Memory - 94.4 MB (67.27%)

--- 0234-palindrome-linked-list/solution.java
+++ 0234-palindrome-linked-list/solution.java
@@ -9,38 +9,33 @@
  * }
  */
 class Solution {
-    public ListNode ReverseLL(ListNode head){
-        ListNode prev=null;
-        ListNode cur=head;
-        while(cur!=null){
-            ListNode nextNode=cur.next;
-            cur.next=prev;
-            prev=cur;
-            cur=nextNode;
-        }
-        return prev;
-    }
     public boolean isPalindrome(ListNode head) {
-        if(head==null || head.next==null){
+        if(head.next==null){
             return true;
         }
-        ListNode slow=head;
         ListNode fast=head;
+        ListNode slow=head;
         while(fast!=null && fast.next!=null){
             slow=slow.next;
             fast=fast.next.next;
         }
-        ListNode secondhead=ReverseLL(slow);
-        ListNode cur=head;
-        ListNode second=secondhead;;
-        while(second!=null){
-            if(cur.val!=second.val){
+        ListNode middle=slow; 
+        ListNode prev=null;
+        ListNode curr=middle;
+        while(curr!=null){
+            ListNode temp=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=temp;
+        }
+        ListNode head2=prev;
+        while(head!=null && head2!=null){
+            if(head.val!=head2.val){
                 return false;
             }
-            cur=cur.next;
-            second=second.next;
+            head=head.next;
+            head2=head2.next;
         }
-        slow=ReverseLL(secondhead);
         return true;
     }
 }
