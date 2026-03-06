26c499824dbfb7d5957711ee1fceb7a8782d00e3
Sujithra S <sujithra.s2023@vitstudent.ac.in>
2026-03-06 09:39:04 +0000

    Sync LeetCode submission Runtime - 2 ms (98.16%), Memory - 43.2 MB (72.68%)

--- 0020-valid-parentheses/solution.java
+++ 0020-valid-parentheses/solution.java
@@ -1,20 +1,26 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> stack=new Stack<>();
-        HashMap<Character,Character> map=new HashMap<>();
-        map.put(')','(');
-        map.put('}','{');
-        map.put(']','[');
-        for(char ch:s.toCharArray()){
-            if(map.containsValue(ch)){
-                stack.push(ch);
+        Stack<Character> st=new Stack<>();
+        for(char c:s.toCharArray()){
+            if(c=='(' || c=='{' || c=='['){
+                st.push(c);
             }
-            else if(map.containsKey(ch)){
-                if(stack.isEmpty() || map.get(ch)!=stack.pop()){
+            else{
+                if(st.isEmpty()){
+                    return false;
+                }
+                char last=st.pop();
+                if(c==')' && last!='('){
+                    return false;
+                }
+                if(c==']' && last!='['){
+                    return false;
+                }
+                if(c=='}' && last!='{'){
                     return false;
                 }
             }
         }
-        return stack.isEmpty();
+        return st.isEmpty();
     }
 }
