02ab5ff6367dd64d173f7bebb52ea3eaef93c58f
Sujithra S <sujithra.s2023@vitstudent.ac.in>
2026-05-14 15:27:50 +0000

    Sync LeetCode submission Runtime - 488 ms (5.04%), Memory - 0.0B (100.00%)

--- 1801-average-time-of-process-per-machine/solution.sql
+++ 1801-average-time-of-process-per-machine/solution.sql
@@ -1,13 +1,10 @@
-SELECT
-    machine_id,
-    ROUND(
-        AVG(
-            CASE
-                WHEN activity_type = 'end' THEN timestamp
-                ELSE -timestamp
-            END
-        )*2
-    , 3) AS processing_time
-FROM Activity
-GROUP BY machine_id;
-
+# Write your MySQL query statement below
+SELECT start.machine_id,
+       ROUND(AVG(end.timestamp - start.timestamp), 3) AS processing_time
+FROM Activity start
+JOIN Activity end
+ON start.machine_id = end.machine_id
+AND start.process_id = end.process_id
+WHERE start.activity_type = 'start'
+AND end.activity_type = 'end'
+GROUP BY start.machine_id;
