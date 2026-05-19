081beb5a850440d2f955971c8704191d9b62778a
Sujithra S <sujithra.s2023@vitstudent.ac.in>
2026-05-19 08:59:27 +0000

    Sync LeetCode submission Runtime - 3 ms (60.54%), Memory - 52.4 MB (65.46%)

--- 0485-max-consecutive-ones/solution.java
+++ 0485-max-consecutive-ones/solution.java
@@ -1,17 +1,17 @@
 class Solution {
     public int findMaxConsecutiveOnes(int[] nums) {
-        int maxcount=0;int count=0;
+        int count=0;
+        int maxcount=0;
         for(int i=0;i<nums.length;i++){
             if(nums[i]==1){
                 count=count+1;
                 if(count>maxcount){
-                maxcount=count;
+                    maxcount=count;
                 }
             }
             else{
                 count=0;
             }
-            
         }
         return maxcount;
     }
