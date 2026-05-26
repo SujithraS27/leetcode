368ed61e1a6570ff1a2d8c61b8310317ca817ede
Sujithra S <sujithra.s2023@vitstudent.ac.in>
2026-05-26 13:19:20 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 43.5 MB (88.23%)

--- 0088-merge-sorted-array/solution.java
+++ 0088-merge-sorted-array/solution.java
@@ -6,15 +6,14 @@ class Solution {
         while(i>=0 && j>=0){
             if(nums1[i]>nums2[j]){
                 nums1[k]=nums1[i];
-                i--;
                 k--;
+                i--;
             }
             else{
                 nums1[k]=nums2[j];
-                j--;
                 k--;
+                j--;
             }
-            //k--;
         }
         while(j>=0){
             nums1[k]=nums2[j];
