615cafcff4e210c2de29af788c142b97e06e2016
Sujithra S <sujithra.s2023@vitstudent.ac.in>
2026-06-03 16:17:46 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 46.7 MB (49.51%)

--- 0142-linked-list-cycle-ii/solution.java
+++ 0142-linked-list-cycle-ii/solution.java
@@ -11,29 +11,25 @@
  */
 public class Solution {
     public ListNode detectCycle(ListNode head) {
-        ListNode fast=head;
-        ListNode slow=head;
-        if(head==null || head.next==null){
-            return null;
-        }
-        while(fast!=null && fast.next!=null){
-            slow=slow.next;
-            fast=fast.next.next;
-            if(slow==fast){//cycle detected
-                break;
-            }
-        }
-        //no cycle
-        while(fast==null || fast.next==null){
-            return null;
-        }
-        slow=head;
-        while(slow!=fast){
-            slow=slow.next;
-            fast=fast.next;
-        }
-        return slow;
+    ListNode slow = head;
+    ListNode fast = head;
 
+    while (fast != null && fast.next != null) {
+        slow = slow.next;
+        fast = fast.next.next;
+
+        if (slow == fast) {
+            ListNode ptr = head;
+
+            while (ptr != slow) {
+                ptr = ptr.next;
+                slow = slow.next;
+            }
+
+            return ptr;
         }
     }
 
+    return null;       
+    }
+}
