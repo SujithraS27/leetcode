1f2aa4d120e29a54e08c2c2987577f9b7d89bb3a
Sujithra S <sujithra.s2023@vitstudent.ac.in>
2026-06-04 08:52:05 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 43.6 MB (20.46%)

--- 0019-remove-nth-node-from-end-of-list/solution.java
+++ 0019-remove-nth-node-from-end-of-list/solution.java
@@ -12,14 +12,14 @@ class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
         ListNode dummy=new ListNode(0);
         dummy.next=head;
-        ListNode fast=dummy;
         ListNode slow=dummy;
-        for(int i=0;i<=n;i++){
+        ListNode fast=dummy;
+        for(int i=0;i<n;i++){
             fast=fast.next;
         }
-        while(fast!=null){
-            fast=fast.next;
+        while(fast.next!=null){
             slow=slow.next;
+            fast=fast.next;
         }
         slow.next=slow.next.next;
         return dummy.next;
