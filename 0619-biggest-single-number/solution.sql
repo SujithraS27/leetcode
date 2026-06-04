b2bcdb2699339db438c5e3a227462a96b611dba0
Sujithra S <sujithra.s2023@vitstudent.ac.in>
2026-06-04 15:15:06 +0000

    Sync LeetCode submission Runtime - 451 ms (47.57%), Memory - 0.0B (100.00%)

--- 0619-biggest-single-number/solution.sql
+++ 0619-biggest-single-number/solution.sql
@@ -1,10 +1,2 @@
-SELECT 
-  MAX(num) AS num
-FROM (
-  SELECT num
-  FROM MyNumbers
-  GROUP BY num
-  HAVING COUNT(*) = 1
-) as singles
-
-
+# Write your MySQL query statement below
+SELECT MAX(num) AS num FROM (SELECT num,COUNT(*) FROM MyNumbers GROUP BY num HAVING COUNT(*)=1)t
