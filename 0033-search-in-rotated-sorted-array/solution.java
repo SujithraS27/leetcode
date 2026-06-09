b2314da10ea67ff9ae9e4e7fc74ef6df5787db89
Sujithra S <sujithra.s2023@vitstudent.ac.in>
2026-06-09 08:06:10 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 43.7 MB (64.82%)

--- 0033-search-in-rotated-sorted-array/solution.java
+++ 0033-search-in-rotated-sorted-array/solution.java
@@ -1,57 +1,26 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int pivot=pivot(nums);
-        if(pivot==-1){
-            return binarysearch(nums,target,0,nums.length-1);
-        }
-       
-        if(nums[pivot]==target){
-            return pivot;
-        }
-        
-        
-
-        if(target<nums[0]){
-            return binarysearch(nums,target,pivot+1,nums.length-1);
-        }
-        if(target>=nums[0]){
-            return binarysearch(nums,target,0,pivot-1);
-        }
-        
-        return -1;
-    }
-    int binarysearch(int[] nums,int target,int start,int end){ 
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-
-            if (nums[mid] == target) {
-                return mid;
-            } else if (nums[mid] < target) {
-                start = mid + 1;
-            } else {
-                end = mid - 1;
+        int low=0;
+        int high=nums.length-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if (nums[mid] == target) return mid;
+            if(nums[low]<=nums[mid]){
+                if(nums[low]<=target && target<nums[mid]){
+                    high=mid-1;
+                }
+                else{
+                    low=mid+1;
+                }
             }
-        }
-
-        return -1; 
-    }
-    int pivot(int nums[]){
-        int start=0;
-        int end=nums.length-1;
-        while(start<=end){
-        int mid=start+(end-start)/2;
-        if((mid<end) && (nums[mid]>nums[mid+1])){
-            return mid;
-        }
-        if((mid>start)&&(nums[mid]<nums[mid-1])){
-            return mid-1;
-        }
-        if(nums[start]>=nums[mid]){
-            end=mid-1;
-        }
-        if(nums[start]<nums[mid]){
-            start=mid+1;
-        }
+            else{
+                if(nums[mid]<target && target<=nums[high]){
+                    low=mid+1;
+                }
+                else{
+                    high=mid-1;
+                }
+            }
         }
         return -1;
     }
