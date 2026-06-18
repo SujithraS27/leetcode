1fa7569b2c4c19768d18c0f79eb8537230c4cf8d
Sujithra S <sujithra.s2023@vitstudent.ac.in>
2026-06-18 17:10:26 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 43.4 MB (15.54%)

--- 0145-binary-tree-postorder-traversal/solution.java
+++ 0145-binary-tree-postorder-traversal/solution.java
@@ -14,17 +14,17 @@
  * }
  */
 class Solution {
-    public void postorder(TreeNode root,List<Integer> result){
+    List<Integer> ans=new ArrayList<>();
+    public List<Integer> postorderTraversal(TreeNode root) {
+        postorder(root);
+        return ans;
+    }
+    public void postorder(TreeNode root){
         if(root==null){
             return;
         }
-        postorder(root.left,result);
-        postorder(root.right,result);
-        result.add(root.val);
-    }
-    public List<Integer> postorderTraversal(TreeNode root) {
-        List<Integer> result=new ArrayList<>();
-        postorder(root,result);
-        return result;
+        postorder(root.left);
+        postorder(root.right);
+        ans.add(root.val);
     }
 }
