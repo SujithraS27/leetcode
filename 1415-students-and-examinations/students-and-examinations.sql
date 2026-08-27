b487262bd1459da43f867eed503b189e66a4c2e0
Sujithra S <sujithra.s2023@vitstudent.ac.in>
2026-08-27 14:53:58 +0530

    Time: 974 ms (86.45%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1415-students-and-examinations/students-and-examinations.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+SELECT s.student_id,s.student_name,su.subject_name,COUNT(e.subject_name) AS attended_exams FROM Students s CROSS JOIN Subjects su LEFT JOIN Examinations e ON e.subject_name=su.subject_name AND s.student_id=e.student_id GROUP BY s.student_id,su.subject_name ORDER BY s.student_id,su.subject_name
